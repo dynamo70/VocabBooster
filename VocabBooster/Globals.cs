6f6f48d0576cb61717b5723e8fd501ee71567f07
dynamo70 <sbasalan@gmail.com>
2014-12-08 14:35:28 +0700

    Latest (2014-12-08)

--- VocabBooster/Globals.cs
+++ VocabBooster/Globals.cs
@@ -20,10 +20,19 @@ namespace VocabBooster
 			public static string strTxtFile = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\MyWordsToLearn.txt";
 			public static string strRemove = string.Empty;
 
+			// to get executable file name from MainWindow
+			public static string strShortCutName = string.Empty;
+
+			// to get my program fullpath with its name to run it by shortcut
+			public static string strTarget = string.Empty;
+
 			// counter of rows in database for swapping in DataGridView1
 			public static int iDataRowCountSwap_DGV1 = 0;
 			public static int iDataRowCountSwap_DGV2 = 0;
 			public static int iDataRowCountSwap_DGV3 = 0;
+
+			// To check count of data greater than 6 for random popup form
+			public static bool checkCountOfData;             
 
 			public static string strLearned = string.Empty;
 		}
